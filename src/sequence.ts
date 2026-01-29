e502beededf52c7be912458cc0563ba4f8600272
Aset Telmanov <telmanov@aktau.rss.ru>
2026-01-29 18:41:56 +0500

    Refactor sequence module: add cycleQuard, zip, batch, flat, min, and max functions; improve existing function signatures and error handling

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,57 +1,93 @@
 import type {
+  Comparator,
   Consumer,
   Mapper,
   Predicate,
   Reducer,
 } from "./types"
 
-export function isIterable<T>(obj: unknown): obj is Iterable<T> {
+export function isIterable<T>(
+  obj: unknown
+): obj is Iterable<T> {
   return (obj != null) && (typeof (obj as any)[Symbol.iterator] === "function")
 }
 
-export function* entries<T>(iterable: Iterable<T>) {
+export function* cycleQuard<T extends object>(
+  iterable: Iterable<T>
+): IterableIterator<T> {
+  if (!isIterable(iterable))
+    throw new TypeError("The iterable is not iterable.")
+
+  const visited = new Set<T>()
+  for (const element of iterable) {
+    if (visited.has(element))
+      throw new Error(
+        "Cycle detected: The iterable contains a circular reference.")
+    visited.add(element)
+    yield element
+  }
+}
+
+export function* entries<T>(
+  iterable: Iterable<T>
+): IterableIterator<[number, T]> {
   let index = 0
   for (const element of iterable)
     yield [index++, element]
 }
 
-export function* values<T>(iterable: Iterable<T>) {
+export function* values<T>(
+  iterable: Iterable<T>
+): IterableIterator<T> {
   for (const element of iterable)
     yield element
 }
 
-export function* keys<T>(iterable: Iterable<T>) {
+export function* keys<T>(
+  iterable: Iterable<T>
+): IterableIterator<number> {
   let index = 0
   for (const element of iterable)
     yield index++
 }
 
-export function length<T>(iterable: Iterable<T>) {
+export function length<T>(
+  iterable: Iterable<T>
+): number {
   let count = 0
   for (const _ of iterable)
     count++
   return count
 }
 
-export function first<T>(iterable: Iterable<T>) {
-  for (const element of iterable)
-    return element
+export function first<T>(
+  iterable: Iterable<T>
+): T | undefined {
+  return iterable[Symbol.iterator]().next().value
 }
 
-export function last<T>(iterable: Iterable<T>) {
+export function last<T>(
+  iterable: Iterable<T>
+): T | undefined {
   let last
   for (const element of iterable)
     last = element
   return last
 }
 
-export function forEach<T>(iterable: Iterable<T>, callback: Consumer<T>) {
+export function forEach<T>(
+  iterable: Iterable<T>,
+  callback: Consumer<T>
+): void {
   let index = 0
   for (const element of iterable)
     callback(element, index++, iterable)
 }
 
-export function some<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
+export function some<T>(
+  iterable: Iterable<T>,
+  predicate: Predicate<T>
+): boolean {
   let index = 0
   for (const element of iterable) {
     if (predicate(element, index++, iterable))
@@ -60,7 +96,10 @@ export function some<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
   return false
 }
 
-export function every<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
+export function every<T>(
+  iterable: Iterable<T>,
+  predicate: Predicate<T>
+): boolean {
   let index = 0
   for (const element of iterable) {
     if (!predicate(element, index++, iterable))
@@ -69,7 +108,10 @@ export function every<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
   return true
 }
 
-export function find<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
+export function find<T>(
+  iterable: Iterable<T>,
+  predicate: Predicate<T>
+): T | undefined {
   let index = 0
   for (const element of iterable) {
     if (predicate(element, index++, iterable))
@@ -77,7 +119,10 @@ export function find<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
   }
 }
 
-export function findIndex<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
+export function findIndex<T>(
+  iterable: Iterable<T>,
+  predicate: Predicate<T>
+): number {
   let index = 0
   for (const element of iterable) {
     if (predicate(element, index++, iterable))
@@ -86,14 +131,20 @@ export function findIndex<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
   return -1
 }
 
-export function at<T>(iterable: Iterable<T>, index: number) {
+export function at<T>(
+  iterable: Iterable<T>,
+  index: number
+): T | undefined {
   let i = 0
   for (const element of iterable)
     if (i++ === index)
       return element
 }
 
-export function includes<T>(iterable: Iterable<T>, item: T) {
+export function includes<T>(
+  iterable: Iterable<T>,
+  item: T
+): boolean {
   for (const element of iterable) {
     if (element === item)
       return true
@@ -101,7 +152,10 @@ export function includes<T>(iterable: Iterable<T>, item: T) {
   return false
 }
 
-export function indexOf<T>(iterable: Iterable<T>, item: T) {
+export function indexOf<T>(
+  iterable: Iterable<T>,
+  item: T
+): number {
   let index = -1
   for (const element of iterable) {
     index++
@@ -111,20 +165,30 @@ export function indexOf<T>(iterable: Iterable<T>, item: T) {
   return -1
 }
 
-export function* filter<T>(iterable: Iterable<T>, predicate: Predicate<T>) {
+export function* filter<T>(
+  iterable: Iterable<T>,
+  predicate: Predicate<T>
+): IterableIterator<T> {
   let index = 0
   for (const element of iterable)
     if (predicate(element, index++, iterable))
       yield element
 }
 
-export function* map<T, U>(iterable: Iterable<T>, mapper: Mapper<T, U>) {
+export function* map<T, U>(
+  iterable: Iterable<T>,
+  mapper: Mapper<T, U>
+): IterableIterator<U> {
   let index = 0
   for (const element of iterable)
-    yield mapper(element, index++, iterable) as U
+    yield mapper(element, index++, iterable)
 }
 
-export function* slice<T>(iterable: Iterable<T>, start: number, end?: number) {
+export function* slice<T>(
+  iterable: Iterable<T>,
+  start: number,
+  end?: number
+): IterableIterator<T> {
   let index = 0
 
   for (const element of iterable) {
@@ -162,11 +226,94 @@ export function reduce<T, U = T>(
   }
 }
 
-export function* take<T>(iterable: Iterable<T>, count: number) {
+export function* take<T>(
+  iterable: Iterable<T>,
+  count: number
+): IterableIterator<T> {
   let index = 0
   for (const element of iterable) {
     if (++index > count)
       break
     yield element
   }
+}
+
+export function* zip<T, U>(
+  iterable1: Iterable<T>,
+  iterable2: Iterable<U>
+): IterableIterator<[T, U]> {
+  const iterator1 = iterable1[Symbol.iterator]()
+  const iterator2 = iterable2[Symbol.iterator]()
+  while (true) {
+    const { value: value1, done: done1 } = iterator1.next()
+    const { value: value2, done: done2 } = iterator2.next()
+    if (done1 || done2)
+      return
+    yield [value1, value2]
+  }
+}
+
+export function* batch<T>(
+  iterable: Iterable<T>,
+  size: number
+): IterableIterator<T[]> {
+  let group: T[] = []
+  for (const element of iterable) {
+    group.push(element)
+    if (group.length === size) {
+      yield group
+      group = []
+    }
+  }
+  if (group.length > 0)
+    yield group
+}
+
+export function* flat<T>(
+  iterable: Iterable<T | Iterable<T>>,
+  depth?: number
+): IterableIterator<T> {
+  if (depth == null)
+    depth = 1
+
+  for (const element of iterable) {
+    if (isIterable(element)) {
+      if (depth > 1)
+        yield* flat(element, depth - 1)
+      else
+        yield* element
+    } else
+      yield element
+  }
+}
+
+const defaultComparator = <T>(a: T, b: T) => (a < b ? -1 : a > b ? 1 : 0)
+export function min<T>(
+  iterable: Iterable<T>,
+  comparator?: Comparator<T>
+): T | undefined {
+  if (comparator == null)
+    comparator = defaultComparator
+
+  let min
+  for (const element of iterable) {
+    if ((min == null) || comparator(min, element) > 0)
+      min = element
+  }
+  return min
+}
+
+export function max<T>(
+  iterable: Iterable<T>,
+  comparator?: Comparator<T>
+): T | undefined {
+  if (comparator == null)
+    comparator = defaultComparator
+
+  let max
+  for (const element of iterable) {
+    if ((max == null) || comparator(max, element) < 0)
+      max = element
+  }
+  return max
 }
